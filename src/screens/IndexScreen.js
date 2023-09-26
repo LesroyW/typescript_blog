94202bb17c0dd42cedfc119bbe960e189914d0a3
Lesroy Wedderburn <wlesroy98@outlook.com>
2023-09-26 17:21:19 +0100

    Adding state in context for tutorial app

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,17 +1,18 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import BlogContext from "../context/BlogContext";
 
 
 const IndexScreen = () => {
 
-    const blogsPosts = useContext(BlogContext);
+    const { data, addBlogPost} = useContext(BlogContext);
 
     return (
         <View>
             <Text>Index Screen</Text>
+            <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList 
-            data={blogsPosts} 
+            data={data} 
             keyExtractor={(blogsPosts) => blogsPosts.title}
             renderItem={({ item }) => {
                 return <Text>{item.title}</Text>
