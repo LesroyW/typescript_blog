8af91be1dbabb9f31fc3f7fff3fabb114b590fdb
Lesroy Wedderburn <wlesroy98@outlook.com>
2023-10-18 17:23:24 +0100

    Updating context for reducers

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
-import BlogContext from "../context/BlogContext";
+import { Context } from "../context/BlogContext";
 
 
 const IndexScreen = () => {
 
-    const { data, addBlogPost} = useContext(BlogContext);
+    const { data, addBlogPost} = useContext(Context);
 
     return (
         <View>
